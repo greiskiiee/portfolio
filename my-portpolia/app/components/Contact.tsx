acbc58e05fcd2aac3f5c48a509f25b0bdae19105
chimegerdene <chimgeebatuh@gmail.com>
2025-10-28 17:01:06 +0800

    contact

--- my-portpolia/app/components/Contact.tsx
+++ my-portpolia/app/components/Contact.tsx
@@ -10,7 +10,7 @@ export const Contact = () => {
         {t.contact_title}
       </h2>
       <p className="text-[#374375] mb-6">{t.contact_text}</p>
-      <div className="flex gap-2 justify-center items-center">
+      <div className="flex flex-wrap gap-2 justify-center items-center">
         <a
           href="mailto:chimgeebatuh@gmail.com"
           className="flex gap-2 bg-[#dfaea0] text-white px-6 py-3 rounded-full hover:bg-[#895159]  transition"
